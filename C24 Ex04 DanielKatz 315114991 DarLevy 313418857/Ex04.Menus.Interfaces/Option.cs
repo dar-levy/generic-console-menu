8f71d952d1aa0d1c2e8faee4e460b59dd9ad00cd
Dar Levy <darlevy8@gmail.com>
2024-09-12 15:22:24 +0300

    Create preliminary fields and constructor

--- C24 Ex04 DanielKatz 315114991 DarLevy 313418857/Ex04.Menus.Interfaces/Option.cs
+++ C24 Ex04 DanielKatz 315114991 DarLevy 313418857/Ex04.Menus.Interfaces/Option.cs
@@ -2,5 +2,37 @@
 
 public class Option
 {
-        
+    private readonly string r_Description;
+    private readonly List<Option> r_Options = new List<Option>();
+    private Option m_ParentMenu;
+
+    public Option(string i_Description, Option i_ParentMenu)
+    {
+        r_Description = i_Description;
+        m_ParentMenu = i_ParentMenu;
+    }
+
+    public int OptionsCount
+    {
+        get 
+        { 
+            return r_Options.Count; 
+        }
+    }
+
+    public List<Option> Options
+    {
+        get 
+        {
+            return r_Options; 
+        }
+    }
+
+    public string Description
+    {
+        get 
+        {
+            return r_Description;
+        }
+    }
 }
