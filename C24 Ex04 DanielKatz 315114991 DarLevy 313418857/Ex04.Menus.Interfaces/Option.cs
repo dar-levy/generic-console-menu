15b8b2728cc43474f7d5357d028f0416ed83e247
Dar Levy <darlevy8@gmail.com>
2024-09-12 16:07:39 +0300

    Create ExecuteSubOption

--- C24 Ex04 DanielKatz 315114991 DarLevy 313418857/Ex04.Menus.Interfaces/Option.cs
+++ C24 Ex04 DanielKatz 315114991 DarLevy 313418857/Ex04.Menus.Interfaces/Option.cs
@@ -33,6 +33,22 @@ public class Option
         }
     }
     
+    public IFunctionality Funtionality
+    {
+        get 
+        {
+            return m_Functionality;
+        }
+    }
+
+    public int Count
+    {
+        get
+        {
+            return r_Options.Count;
+        }
+    }
+    
     public void AddOption(string i_Description, IFunctionality i_Functionality)
     {
         r_Options.Add(new Option(i_Description, this, i_Functionality));
@@ -50,5 +66,26 @@ public class Option
         menuOutput.Append(m_Parent == null ? "\n0 - Exit" : "\n0 - Back");
 
         Console.WriteLine(menuOutput);
+    }
+    
+    public Option ExecuteSubOption(int i_Choice)
+    {
+        Console.Clear();
+
+        if (i_Choice == 0)
+        {
+            return m_Parent;
+        }
+
+        Option selectedOption = r_Options[i_Choice];
+
+        if (selectedOption.Count > 0)
+        {
+            return selectedOption;
+        }
+
+        selectedOption.Funtionality.Execute();
+        
+        return this;
     }
 }
