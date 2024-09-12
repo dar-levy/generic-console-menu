50ec64e9ff43cde346cad79df94ed1fde6f13e03
Dar Levy <darlevy8@gmail.com>
2024-09-12 23:12:07 +0300

    Rename event to interface (mistake)

--- C24 Ex04 DanielKatz 315114991 DarLevy 313418857/C24 Ex04 DanielKatz 315114991 DarLevy 313418857/MenuTests.cs
+++ C24 Ex04 DanielKatz 315114991 DarLevy 313418857/C24 Ex04 DanielKatz 315114991 DarLevy 313418857/MenuTests.cs
@@ -1,65 +1,58 @@
 
-using Ex04.Menus.Interfaces;
+using Interfaces = Ex04.Menus.Interfaces;
+using Events = Ex04.Menus.Events;
 
-namespace C24_Ex04_DanielKatz_315114991_DarLevy_313418857;
 
-public class MenuTests
+namespace C24_Ex04_DanielKatz_315114991_DarLevy_313418857
 {
-    	public readonly MainMenu m_MainMenuInterface = new MainMenu();
-		// public readonly Delegates.MainMenu m_MainMenuDelegate = new Delegates.MainMenu();
+	public class MenuTests
+	{
+		public readonly Interfaces.MainMenu m_MainMenuInterface = new Interfaces.MainMenu();
+		public readonly Events.MainMenu m_MainMenuEvent = new Events.MainMenu();
 
-		// Members for Interface menu
-		private Option m_ShowDateTimeMenuInterface;
-		private Option m_VersionAndDigitsMenInterface;
+		private Interfaces.Option m_ShowDateTimeMenuInterface;
+		private Interfaces.Option m_VersionAndDigitsMenuInterface;
 
-		// // Members for Delegate menu
-		// private Delegates.MenuItem m_ShowDateTimeMenuDelegate;
-		// private Delegates.MenuActionItem m_ShowTimeDelegate;
-		// private Delegates.MenuActionItem m_ShowDateDelegate;
-		// private Delegates.MenuItem m_VersionAndDigitsMenDelegate;
-		// private Delegates.MenuActionItem m_CountDigitsDelegate;
-		// private Delegates.MenuActionItem m_ShowVersionDelegate;
+		private Events.Option m_ShowDateTimeMenuDelegate;
+		private Events.Option m_VersionAndDigitsMenDelegate;
 
 		public MenuTests()
 		{
 			buildMenuInterfaces();
-			// buildMenuDelegates();
+			buildMenuDelegates();
 		}
 
 		private void buildMenuInterfaces()
 		{
 			//Create the Interface Main Menu
-			m_ShowDateTimeMenuInterface = new Option(m_MainMenuInterface.Root, "Show Date/Time");
-			m_ShowDateTimeMenuInterface.AddOption(new Option(m_ShowDateTimeMenuInterface, "Show Time", new TimePresentor()));
-			m_ShowDateTimeMenuInterface.AddOption(new Option(m_ShowDateTimeMenuInterface, "Show Date", new DatePresentor()));
-			
-			m_VersionAndDigitsMenInterface = new Option(m_MainMenuInterface.Root, "Version and Capitals");
-			m_VersionAndDigitsMenInterface.AddOption(new Option(m_VersionAndDigitsMenInterface, "Count Capitals", new CapitalsCounter()));
-			m_VersionAndDigitsMenInterface.AddOption(new Option(m_VersionAndDigitsMenInterface, "Show Version", new VersionPresentor()));
-			
+			m_ShowDateTimeMenuInterface = new Interfaces.Option(m_MainMenuInterface.Root, "Show current Date/Time");
+			m_ShowDateTimeMenuInterface.AddOption(new Interfaces.Option(m_ShowDateTimeMenuInterface, "Show current Time",new TimePresentor()));
+			m_ShowDateTimeMenuInterface.AddOption(new Interfaces.Option(m_ShowDateTimeMenuInterface, "Show current Date", new DatePresentor()));
+
+			m_VersionAndDigitsMenuInterface = new Interfaces.Option(m_MainMenuInterface.Root, "Version and Capitals");
+			m_VersionAndDigitsMenuInterface.AddOption(new Interfaces.Option(m_VersionAndDigitsMenuInterface, "Count Capitals", new CapitalsCounter()));
+			m_VersionAndDigitsMenuInterface.AddOption(new Interfaces.Option(m_VersionAndDigitsMenuInterface, "Show Version", new VersionPresentor()));
+
 			m_MainMenuInterface.Root.AddOption(m_ShowDateTimeMenuInterface);
-			m_MainMenuInterface.Root.AddOption(m_VersionAndDigitsMenInterface);
+			m_MainMenuInterface.Root.AddOption(m_VersionAndDigitsMenuInterface);
 		}
 
-		// private void buildMenuDelegates()
-		// {
-		// 	//Create the Delegate Main Menu
-		// 	m_ShowDateTimeMenuDelegate = new Delegates.MenuItem(m_MainMenuDelegate.RootMainMenu, "Show Date/Time");
-		// 	m_ShowTimeDelegate = new Delegates.MenuActionItem(m_ShowDateTimeMenuDelegate, "Show Time");
-		// 	m_ShowTimeDelegate.AddFunctionalityAction(UtilsForUI.showTime);
-		// 	m_ShowDateDelegate = new Delegates.MenuActionItem(m_ShowDateTimeMenuDelegate, "Show Date");
-		// 	m_ShowDateDelegate.AddFunctionalityAction(UtilsForUI.showDate);
-		//
-		// 	m_VersionAndDigitsMenDelegate = new Delegates.MenuItem(m_MainMenuDelegate.RootMainMenu, "Version and Digits");
-		// 	m_CountDigitsDelegate = new Delegates.MenuActionItem(m_VersionAndDigitsMenDelegate, "Count Digits");
-		// 	m_CountDigitsDelegate.AddFunctionalityAction(UtilsForUI.showAmountOfcountedDigitsInInput);
-		// 	m_ShowVersionDelegate = new Delegates.MenuActionItem(m_VersionAndDigitsMenDelegate, "Show Version");
-		// 	m_ShowVersionDelegate.AddFunctionalityAction(UtilsForUI.showVersion);
-		// }
+		private void buildMenuDelegates()
+		{
+			m_ShowDateTimeMenuEvents = new Events.Option(m_MainMenuEvent.Root, "Show current Date/Time");
+			m_ShowDateTimeMenuEvents.AddOption(new Events.Option(m_ShowDateTimeMenuEvents, "Show current Time",new TimePresentor()));
+			m_ShowDateTimeMenuEvents.AddOption(new Events.Option(m_ShowDateTimeMenuEvents, "Show current Date", new DatePresentor()));
+
+			m_VersionAndDigitsMenuEvents = new Events.Option(m_MainMenuInterface.Root, "Version and Capitals");
+			m_VersionAndDigitsMenuEvents.AddOption(new Events.Option(m_VersionAndDigitsMenuEvents, "Count Capitals", new CapitalsCounter()));
+			m_VersionAndDigitsMenuEvents.AddOption(new Events.Option(m_VersionAndDigitsMenuEvents, "Show Version", new VersionPresentor()));
+
+			m_MainMenuInterface.Root.AddOption(m_ShowDateTimeMenuEvents);
+			m_MainMenuInterface.Root.AddOption(m_VersionAndDigitsMenuEvents);		}
 		public void Show()
 		{
 			m_MainMenuInterface.Show();
-			// m_MainMenuDelegate.Show();
+			m_MainMenuEvent.Show();
 		}
-
+	}
 }
---
+++ C24 Ex04 DanielKatz 315114991 DarLevy 313418857/Ex04.Menus.Events/MainMenu.cs
@@ -0,0 +1,101 @@
+using System.Text;
+
+namespace Ex04.Menus.Events;
+
+public class MainMenu
+{
+    private readonly string r_Title;
+    private Option m_Root;
+    private Option m_CurrentOption;
+
+    public Option Root
+    {
+        get
+        {
+            return m_Root;
+        }
+
+        set
+        {
+            m_Root = value;
+        }
+    }
+
+    // Methods
+    public MainMenu()
+    {
+        m_Root = new Option(null, "Main Menu");
+        m_CurrentOption = m_Root;
+    }
+  
+    public void Show()
+    {
+        bool isExit = false;
+    
+        while (!isExit)
+        {
+            int choice = Choose();
+            isExit = ShouldExit(choice);
+            bool isBack = ShouldReturn(choice);
+            if (isBack)
+            {
+                m_CurrentOption = m_CurrentOption.Parent;
+            }
+            else if (!isExit && choice > 0)
+            {
+                NavigateToOption(choice - 1);
+            }
+        }
+    }
+
+    private bool ShouldExit(int choice)
+    {
+        return m_CurrentOption == m_Root && choice == 0;
+    }
+
+    private bool ShouldReturn(int choice)
+    {
+        return m_CurrentOption != m_Root && choice == 0;
+    }
+
+    private void NavigateToOption(int choice)
+    {
+        m_CurrentOption = m_CurrentOption.Options[choice];
+    }
+    
+    private int Choose()
+    {
+        int choice = 0;
+        if (m_CurrentOption.IsFunctional())
+        {
+            m_CurrentOption.Execute();
+        }
+        else
+        {
+            m_CurrentOption.Show();
+            choice = getChoice();
+            Console.Clear();
+        }
+
+        return choice;
+    }
+
+    private int getChoice()
+    {
+        int selectedItemMenuIndex;
+
+        while (!isValidChoice(Console.ReadLine(), out selectedItemMenuIndex))
+        {
+            Console.WriteLine("Invalid selection! Please enter a number from the options above, try again:");
+        }
+
+        return selectedItemMenuIndex;
+    }
+
+    private bool isValidChoice(string input, out int selectedItemMenuIndex)
+    {
+        return int.TryParse(input, out selectedItemMenuIndex) &&
+               selectedItemMenuIndex >= 0 &&
+               selectedItemMenuIndex <= m_CurrentOption.Options.Count;
+    }
+}
